0201d2c01b6c392ca416b5b6fcaba170aa2a75ea
inattendu <inattendu@pm.me>
2020-12-05 19:25:11 +0100

    mess with branch

--- server/start.sh
+++ server/start.sh
@@ -2,7 +2,7 @@
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 git pull
-git checkout prod
+git checkout systemd-server
 npm install
 sudo systemctl restart tourniquette
 sudo journalctl -u tourniquette -f
