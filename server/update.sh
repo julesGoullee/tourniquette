7780ecf3374e7aed8a72ec31d534c63f7a15ca0a
inattendu <inattendu@pm.me>
2020-12-05 20:13:31 +0100

    right branch

--- server/update.sh
+++ server/update.sh
@@ -4,7 +4,7 @@ cd ~/tourniquette/server
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 git pull
-git checkout systemd-server
+git checkout prod
 npm install
 sudo /bin/systemctl restart tourniquette
 journalctl -u tourniquette -f
