98028b75a6e07c2643a54f567ff8933334269ceb
jules <julesgoullee@gmail.com>
2020-12-05 21:06:17 +0100

    Prod

---
+++ server/update.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "test"
+cd ~/tourniquette/server
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+git pull
+git checkout prod
+npm install
+sudo /bin/systemctl restart tourniquette
+journalctl -u tourniquette -f
+
