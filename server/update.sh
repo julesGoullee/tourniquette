cb3fda83d138e0a743a4d36a93652d894e241506
inattendu <inattendu@pm.me>
2020-12-05 20:08:44 +0100

    script ok

--- server/update.sh
+++ server/update.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+echo "test"
 cd ~/tourniquette/server
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-npm start
+git pull
+git checkout systemd-server
+npm install
+sudo /bin/systemctl restart tourniquette
 journalctl -u tourniquette -f
 
