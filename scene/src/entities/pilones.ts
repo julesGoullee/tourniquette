473647e43c39b7354a34397e6091404ea6bffe07
julesGoullee <julesgoullee@gmail.com>
2020-12-07 13:11:37 +0100

    revert

--- scene/src/entities/pilones.ts
+++ scene/src/entities/pilones.ts
@@ -8,10 +8,10 @@ export class ThePilones extends Entity {
     this.addComponent(model)
     model.withCollisions = true
 
-    // const myMaterial = new Material()
-    // myMaterial.albedoColor = new Color4(0, 0, 0, 0)
-    // myMaterial.castShadows = false
-    // this.addComponent(myMaterial)
+    const myMaterial = new Material()
+    myMaterial.albedoColor = new Color4(0, 0, 0, 0)
+    myMaterial.castShadows = false
+    this.addComponent(myMaterial)
 
 
     this.addComponent(transform)
